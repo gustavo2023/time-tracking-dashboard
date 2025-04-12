ff6c74102c186d543213b15c8a0f9752e803660f
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-04-11 23:05:25 -0400

    feat: fetch data from JSON and populate DOM after clearing activity cards

--- script.js
+++ script.js
@@ -9,4 +9,14 @@ const clearActivityCards = () => {
   previousHoursElements.forEach((previous) => (previous.textContent = ""));
 };
 
+fetch("./data.json")
+  .then((response) => {
+    if (!response.ok) return console.error("Failed to fetch data.json");
+
+    return response.json();
+  })
+  .then((data) => {
+    populateDOM(data); // TODO: Populate DOM with data
+  });
+
 clearActivityCards();
